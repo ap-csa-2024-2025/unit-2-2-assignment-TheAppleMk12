69e1e9fb365fc95994cee04d74d98b22b0a64ed6
TheAppleMk12 <Abramhtran@gmail.com>
2024-10-17 10:44:55 -0700

    first problem done

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    Scanner sc = new Scanner(System.in);
+    System.out.println("What type of item are you buying?");
+    String item = sc.nextLine();
+    System.out.println(" How many are you buying?");
+    int a = sc.nextInt();
+    System.out.println("How much does each one weigh?");
+    double b = sc.nextDouble();
+    double totalWeight = a*b;
+    System.out.println(a + " " + item + " at " +b +" pounds each will weigh " + totalWeight + " pounds total");
+
   }
 }
